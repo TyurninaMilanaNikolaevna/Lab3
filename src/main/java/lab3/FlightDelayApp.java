dc97eb60e420d9fbd218f4bbfbf845e9a4ced1f6
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-03 12:38:56 +0300

    Scripted auto-commit on change (2021-12-03 12:38:56) by gitwatch.sh

--- src/main/java/lab3/FlightDelayApp.java
+++ src/main/java/lab3/FlightDelayApp.java
@@ -6,12 +6,11 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 public class FlightDelayApp {
 
-    private static String Hadoop;
-
     public static void main(String[] args) throws Exception {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> Flights = sc.textFile(Hadoop "664600583_T_ONTIME_sample.csv");
+        JavaRDD<String> flights = sc.textFile("664600583_T_ONTIME_sample.csv");
+
     }
 }
