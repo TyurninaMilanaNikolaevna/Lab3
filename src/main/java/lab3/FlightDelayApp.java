e3cfaa209cba5a9446e4d69b9f09b53100cd203a
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-03 11:54:44 +0300

    Scripted auto-commit on change (2021-12-03 11:54:44) by gitwatch.sh

--- src/main/java/lab3/FlightDelayApp.java
+++ src/main/java/lab3/FlightDelayApp.java
@@ -1,8 +1,14 @@
 package lab3;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class FlightDelayApp {
 
     public static void main(String[] args) throws Exception {
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
         
     }
 }
