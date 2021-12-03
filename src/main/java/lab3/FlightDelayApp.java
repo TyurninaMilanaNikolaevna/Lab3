6d4bc4b9f14bb3d33312e2b2917d0a26f4cc8cff
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-03 13:54:35 +0300

    Scripted auto-commit on change (2021-12-03 13:54:35) by gitwatch.sh

--- src/main/java/lab3/FlightDelayApp.java
+++ src/main/java/lab3/FlightDelayApp.java
@@ -58,7 +58,7 @@ public class FlightDelayApp {
 
         JavaPairRDD<Tuple2, Serializable> resultInformation = Information.reduceByKey(
                 (x, y) -> {
-                    
+                    float ma
                 }
         )
     }
